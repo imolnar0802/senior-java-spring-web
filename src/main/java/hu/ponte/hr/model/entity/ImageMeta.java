64bf847c2310e251f66e87ede0d77e8cc82656ae
Molnár István <molnar.istvan2@nisz.hu>
2022-09-22 08:09:56 +0200

    create entity

---
+++ src/main/java/hu/ponte/hr/model/entity/ImageMeta.java
@@ -0,0 +1,42 @@
+package hu.ponte.hr.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author zoltan
+ */
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "imageMeta")
+public class ImageMeta
+{
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "mimeType")
+	private String mimeType;
+
+	@Column(name = "size")
+	private long size;
+
+	@Column(name = "digitalSign", length = 512000)
+	private String digitalSign;
+}
